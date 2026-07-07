0afccddc9a4ffdcb3daabb4009d3f48ef6f28b77
Davidmuti <dsmith1978@live.co.uk>
2026-07-07 18:59:25 +0100

    Milestone 1 - Ploppy appears and flies

--- Ploppy Pig/GameScene.swift
+++ Ploppy Pig/GameScene.swift
@@ -1,88 +1,38 @@
-//
-//  GameScene.swift
-//  Ploppy Pig
-//
-//  Created by David Smith on 7/6/26.
-//
-
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+
+    private let ploppy = SKSpriteNode(imageNamed: "ploppy")
+
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
+        removeAllChildren()
+
+        backgroundColor = .cyan
+
+        physicsWorld.gravity = CGVector(dx: 0, dy: -1.5)
+
+        ploppy.position = CGPoint(x: frame.minX + 180,
+                                  y: frame.midY + 100)
+
+        ploppy.setScale(0.08)
+
+        ploppy.physicsBody = SKPhysicsBody(rectangleOf: ploppy.size)
+        ploppy.physicsBody?.allowsRotation = false
+        ploppy.physicsBody?.restitution = 0.2
+
+        addChild(ploppy)
+
+        let ground = SKNode()
+        ground.position = CGPoint(x: frame.midX, y: frame.minY + 40)
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: frame.width, height: 20))
+        ground.physicsBody?.isDynamic = false
+        addChild(ground)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        ploppy.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        ploppy.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 130))
     }
 }
+
